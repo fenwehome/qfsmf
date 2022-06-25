fd64a7658d02cc29d3423630fbd01b89c2ba51a5
四个圈 <fenwe521@gmail.com>
2022-06-25 14:10:40 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://fenwe.coding.net/p/catvod/d/fenwe/git/raw/master/peizhi.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
