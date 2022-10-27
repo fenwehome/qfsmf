e8f8281f2d95988177f4aa9f2f9cc2b3b304a378
四个圈 <fenwe521@gmail.com>
2022-10-27 23:16:24 +0800

    修正系统自带view携带cookie不生效的问题

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -1482,10 +1482,7 @@ public class PlayActivity extends BaseActivity {
 
         @Nullable
         @Override
-        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-            CookieManager cookieManager = CookieManager.getInstance();
-            String CookieStr = cookieManager.getCookie(url);
-            if(CookieStr!=null)webCookieStr=CookieStr;
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) 
             return checkIsVideo(url, null);
         }
 
@@ -1494,6 +1491,9 @@ public class PlayActivity extends BaseActivity {
         @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
             String url = request.getUrl().toString();
+            CookieManager cookieManager = CookieManager.getInstance();
+            String CookieStr = cookieManager.getCookie(url);
+            if(CookieStr!=null)webCookieStr=CookieStr;
             LOG.i("shouldInterceptRequest url:" + url);
             HashMap<String, String> webHeaders = new HashMap<>();
             Map<String, String> hds = request.getRequestHeaders();
